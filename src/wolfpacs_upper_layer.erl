947bffb87e4e2471163a847a49f49b11145d02b7
Niklas Johansson <raphexion@gmail.com>
2019-12-30 07:35:15 +0100

    upper_layer: Refactor to use fsm

--- src/wolfpacs_upper_layer.erl
+++ src/wolfpacs_upper_layer.erl
@@ -9,6 +9,7 @@
 -module(wolfpacs_upper_layer).
 -behaviour(gen_server).
 -include_lib("eunit/include/eunit.hrl").
+-import(wolfpacs_utils, [split/2]).
 
 %% API
 -export([start_link/4]).
@@ -34,13 +35,17 @@ start_link(_Ref, Socket, Transport, Opts) ->
 
 -record(state, {socket,
 		transport,
-		data = <<>>
+		fsm,
+		maxPDU = 65536,
+		data = <<>>,
+		associated = false
 	       }).
 
 %% @hidden
 init([Socket, Transport, _Opts = []]) ->
+    {ok, FSM} = wolfpacs_upper_layer_fsm:start(self()),
     Transport:setopts(Socket, [{active, true}]),
-    {ok, #state{socket=Socket, transport=Transport}}.
+    {ok, #state{socket=Socket, transport=Transport, fsm=FSM}}.
 
 %% @hidden
 handle_call(What, _From, State) ->
@@ -57,25 +62,32 @@ handle_info({tcp_closed, _Port}, State) ->
     lager:debug("connection closed"),
     {stop, normal, State};
 
-handle_info({tcp, _Port, DataNew}, State0=#state{data=DataOld}) ->
+handle_info({tcp, _Port, DataNew}, State0=#state{data=DataOld, fsm=FSM}) ->
     Data = <<DataOld/binary, DataNew/binary>>,
     case protocol_data_unit_complete(Data) of
-	{ok, PDU, Rest} ->
-	    wolfpacs_pdu:handle_pdu(PDU),
+	{ok, PDUType, PDU, Rest} ->
+	    wolfpacs_upper_layer_fsm:pdu(FSM, PDUType, PDU),
 	    {noreply, State0#state{data=Rest}};
 	{error, Data} ->
-	    {noreply, State0#state{data=Data}}
+	    {noreply, State0#state{data=Data}};
+	What ->
+	    lager:warning("error ~p", [What])
     end;
 
 handle_info({handshake, wolfpack, _, _, _}, State) ->
     lager:debug("handshake"),
     {noreply, State};
 
+handle_info({send_response, Payload}, State) ->
+    send_response(Payload, State),
+    {noreply, State};
+
 handle_info(_What, State) ->
     {noreply, State}.
 
 %% @hidden
-terminate(_Reason, _State) ->
+terminate(_Reason, #state{fsm=FSM}) ->
+    wolfpacs_upper_layer_fsm:stop(FSM),
     ok.
 
 %% @hidden
@@ -86,23 +98,24 @@ code_change(_OldVsn, State, _Extra) ->
 %% Private
 %%------------------------------------------------------------------------------
 
-protocol_data_unit_complete(Data = <<1, _, PDUSize:32, Payload/binary>>) ->
-    FullSize = byte_size(Payload),
-    case FullSize >= PDUSize of
-	true ->
-	    PDU = binary:part(Payload, 0, PDUSize),
-	    Rest = binary:part(Payload, PDUSize, FullSize - PDUSize),
-	    {ok, PDU, Rest};
-	false ->
+protocol_data_unit_complete(Data = <<PDUType, _, PDUSize:32, _/binary>>) ->
+    case split(Data, PDUSize + 6) of
+	{ok, PDU, Rest} ->
+	    {ok, PDUType, PDU, Rest};
+	{error, Data} ->
 	    {error, Data}
     end;
 protocol_data_unit_complete(Data) ->
     {error, Data}.
+
+send_response(Payload, #state{socket=Socket, transport=Transport}) ->
+    lager:warning("send response of size ~p", [byte_size(Payload)]),
+    Transport:send(Socket, Payload).
 
 %%------------------------------------------------------------------------------
 %% Test
 %%------------------------------------------------------------------------------
 
 protocol_data_unit_complete_test_() ->
-    [ ?_assert(protocol_data_unit_complete(<<>>) =:= {error, <<>>}),
-      ?_assert(protocol_data_unit_complete(<<1, 5, 2:32, 42, 43, 44, 45>>) =:= {ok, <<42, 43>>, <<44, 45>>})].
+    [ ?_assertEqual(protocol_data_unit_complete(<<>>),  {error, <<>>}),
+      ?_assertEqual(protocol_data_unit_complete(<<1, 5, 2:32, 42, 43, 44, 45, 46>>), {ok, 1, <<1, 5, 2:32, 42, 43>>, <<44, 45, 46>>})].
