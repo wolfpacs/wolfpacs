51ad5e0a4d9041ce043358f1779b9b6335bb4bf6
Niklas Johansson <raphexion@gmail.com>
2019-12-30 06:56:35 +0100

    utils: Add log_to_binary

--- src/wolfpacs_utils.erl
+++ src/wolfpacs_utils.erl
@@ -1,7 +1,9 @@
 -module(wolfpacs_utils).
 -include_lib("eunit/include/eunit.hrl").
 -export([drop_last_byte/1,
-	 split/2]).
+	 split/2,
+	 log_hex_to_int/1,
+	 log_to_binary/1]).
 
 -spec drop_last_byte(binary()) -> binary().
 drop_last_byte(Data) ->
@@ -21,6 +23,42 @@ split(Data, At) when At >= 0 ->
 split(Data, _) ->
     {error, Data}.
 
+log_hex_to_int([L]) ->
+    hv(L);
+log_hex_to_int([H, L]) ->
+    hv(H) * 16 + hv(L).
+
+log_to_binary(LogData) ->
+    Tokens = string:tokens(LogData, ", \n\t"),
+    list_to_binary(lists:map(fun log_hex_to_int/1, Tokens)).
+
+%%------------------------------------------------------------------------------
+%% Private
+%%------------------------------------------------------------------------------
+
+hv($0) -> 0;
+hv($1) -> 1;
+hv($2) -> 2;
+hv($3) -> 3;
+hv($4) -> 4;
+hv($5) -> 5;
+hv($6) -> 6;
+hv($7) -> 7;
+hv($8) -> 8;
+hv($9) -> 9;
+hv($A) -> 10;
+hv($B) -> 11;
+hv($C) -> 12;
+hv($D) -> 13;
+hv($E) -> 14;
+hv($F) -> 15;
+hv($a) -> 10;
+hv($b) -> 11;
+hv($c) -> 12;
+hv($d) -> 13;
+hv($e) -> 14;
+hv($f) -> 15.
+
 %%------------------------------------------------------------------------------
 %% Test
 %%------------------------------------------------------------------------------
@@ -35,3 +73,11 @@ split_test_() ->
      ?_assert(split(<<1, 2, 3, 4>>, 2) =:= {ok, <<1 ,2>>, <<3, 4>>}),
      ?_assert(split(<<1, 2, 3, 4>>, 1) =:= {ok, <<1>>, <<2, 3, 4>>})
     ].
+
+log_to_binary_test_() ->
+    Value0 = "31, 2e, 32, 2e, 38, 34, 30, 2e, 31, 30, 30, 30, 38, 2e, 31, 2e, 32",
+    Value1 = "31 2e 32 2e 38 34 30 2e 31 30 30 30 38 2e 31 2e 32",
+    Correct0 = <<"1.2.840.10008.1.2">>,
+    Correct1 = Correct0,
+    [ ?_assertEqual(log_to_binary(Value0), Correct0),
+      ?_assertEqual(log_to_binary(Value1), Correct1) ].
