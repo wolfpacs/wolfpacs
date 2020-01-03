328c1a0a1234f1da648abfba80ada833d62ede51
Niklas Johansson <raphexion@gmail.com>
2020-01-03 20:43:19 +0100

    c_echo_scp_poc_SUITE: Add test against storescu (test data)

--- test/c_echo_scp_poc_SUITE.erl
+++ test/c_echo_scp_poc_SUITE.erl
@@ -4,19 +4,26 @@
 -export([all/0,
 	 init_per_suite/1,
 	 end_per_suite/1]).
--export([test_c_echo_scp_scu/1]).
+-export([test_c_echo_scp_scu/1,
+	 test_storescu_associate_rq/1]).
 
-all() -> [test_c_echo_scp_scu].
+all() -> [test_c_echo_scp_scu,
+	  test_storescu_associate_rq].
 
 init_per_suite(Cfg) ->
     lager_common_test_backend:bounce(debug),
+    application:ensure_all_started(wolfpacs),
     Cfg.
 
 end_per_suite(Cfg) ->
+    application:stop(wolfpacs),
     Cfg.
 
 test_c_echo_scp_scu(_Config) ->
-    application:ensure_all_started(wolfpacs),
     {ok, Echo} = wolfpacs_c_echo_scu:start_link(),
-    {ok, success} = wolfpacs_c_echo_scu:echo(Echo, "localhost", 11112, <<"testtest">>),
-    application:stop(wolfpacs).
+    {ok, success} = wolfpacs_c_echo_scu:echo(Echo, "localhost", 11112, <<"testtest">>).
+
+test_storescu_associate_rq(Config) ->
+    Filename = filename:join([?config(data_dir, Config), "storescu_associate_rq.bin"]),
+    {ok, VariableItems} = file:read_file(Filename),
+    {ok, _Contexts, _MaxSize, _Class, _VersionName, <<>>} = wolfpacs_variable_items_request:decode(VariableItems).
