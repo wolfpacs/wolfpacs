7b0337be1795a4a5e2489a90caf5c1e1aca22c07
Niklas Johansson <raphexion@gmail.com>
2020-01-03 16:31:35 +0100

    Add decode for variable item accept

--- src/wolfpacs_variable_items_accept.erl
+++ src/wolfpacs_variable_items_accept.erl
@@ -13,13 +13,31 @@ encode(PrCID, TransferSyntax, MaxPDUSize, Class, VersionName) ->
 		UserInformation/binary>>,
     Payload.
 
-%% TODO
 decode(Data) ->
-    {error, Data}.
+    decode_with_name(Data, wolfpacs_application_context_name:decode(Data)).
 
 %%==============================================================================
 %% Private
 %%==============================================================================
+
+decode_with_name(OrgData, {error, _}) ->
+    lager:warning("unable to decode_with_name"),
+    {error, OrgData};
+decode_with_name(OrgData, {ok, Name, Rest}) ->
+    decode_with_accept(OrgData, Name, wolfpacs_presentation_context_accept:decode(Rest)).
+
+decode_with_accept(OrgData, _, {error, _}) ->
+    lager:warning("unable to decode_with_accept"),
+    {error, OrgData};
+decode_with_accept(OrgData, Name, {ok, PrCID, TransferSyntax, Rest}) ->
+    MaybeUserInformation = wolfpacs_user_information:decode(Rest),
+    decode_with_user_information(OrgData, Name, PrCID, TransferSyntax, MaybeUserInformation).
+
+decode_with_user_information(OrgData, _Name, _PrCID, _TransferSyntax, {error, _}) ->
+    lager:warning("unable to decode_with_user_information"),
+    {error, OrgData};
+decode_with_user_information(_OrgData, Name, PrCID, TransferSyntax, {ok, MaxSize, ImplementationClass, VersionName, Rest}) ->
+    {ok, Name, PrCID, TransferSyntax, MaxSize, ImplementationClass, VersionName, Rest}.
 
 %%==============================================================================
 %% Test
@@ -44,3 +62,24 @@ D:   30  2e  33  2e  30  2e  33  2e  36  2e  34  55  00  00  0f  4f
 D:   46  46  49  53  5f  44  43  4d  54  4b  5f  33  36  34
 "),
     ?assertEqual(encode(PrCID, TransferSyntax, MaxPDUSize, Class, VersionName), Data).
+
+encode_decode_test_() ->
+    Fixed = <<"1.2.840.10008.3.1.1.1">>,
+    PrCID = 1,
+    TransferSyntax = <<"1.2.840.10008.1.2">>,
+    MaxPDUSize = 16384,
+    Class = <<"1.2.276.0.7230010.3.0.3.6.4">>,
+    VersionName = <<"OFFIS_DCMTK_364">>,
+
+    Encoded0 = encode(PrCID, TransferSyntax, MaxPDUSize, Class, VersionName),
+    Encoded1 = <<Encoded0/binary, 42>>,
+    Incorrect0 = wolfpacs_utils:drop_last_byte(Encoded0),
+    Incorrect1 = <<1, 2, 3, 4, 5>>,
+
+    Correct0 = {ok, Fixed, PrCID, TransferSyntax, MaxPDUSize, Class, VersionName, <<>>},
+    Correct1 = {ok, Fixed, PrCID, TransferSyntax, MaxPDUSize, Class, VersionName, <<42>>},
+
+    [?_assertEqual(decode(Encoded0), Correct0),
+     ?_assertEqual(decode(Encoded1), Correct1),
+     ?_assertEqual(decode(Incorrect0), {error, Incorrect0}),
+     ?_assertEqual(decode(Incorrect1), {error, Incorrect1})].
