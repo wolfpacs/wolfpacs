fb4710f5eb4258e3e2808ef454298d0974d74d46
Niklas Johansson <raphexion@gmail.com>
2020-01-03 16:33:42 +0100

    Add eunit test c_echo_scp

--- src/wolfpacs_c_echo_scp.erl
+++ src/wolfpacs_c_echo_scp.erl
@@ -24,3 +24,26 @@ react_with_info(#{?UID := UID, ?RQID := RQID}) ->
 	     {?CMD, ?STU, us, 16#0000}],
     wolfpacs_dimse_protocol:attach_header(
       wolfpacs_dimse_protocol:encode(Items)).
+
+%%==============================================================================
+%% Test
+%%==============================================================================
+
+-include_lib("eunit/include/eunit.hrl").
+
+react_test() ->
+    UID = <<"1.2.3.4">>,
+    MessageFields = [{?CMD, ?UID, byte_size(UID), UID},
+		     {?CMD, ?RQID, 4, 1}],
+
+    Correct = <<0, 0, 0, 0, 4,
+		0, 0, 0, 56,
+		0, 0, 0, 0,
+		0, 2, 0, 8, 0,
+		0, 0,
+		49, 46, 50, 46, 51, 46, 52, 32,
+		0, 0, 0, 1, 2, 0, 0, 0,
+		48, 128, 0, 0, 32, 1, 2,
+		0, 0, 0, 1, 0, 0, 0, 0, 8, 2,
+		0, 0, 0, 1, 1, 0, 0, 0, 9, 2, 0, 0, 0, 0, 0>>,
+    ?assertEqual(react({ok, MessageFields}), Correct).
