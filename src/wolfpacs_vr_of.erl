753b0ae4b4255bef453d1eeac40ae4fd54136c43
Niklas Johansson <raphexion@gmail.com>
2020-01-19 20:44:32 +0100

    vr_of: Add big endian support

--- src/wolfpacs_vr_of.erl
+++ src/wolfpacs_vr_of.erl
@@ -6,7 +6,9 @@
 
 -module(wolfpacs_vr_of).
 -export([encode_little/1,
-	 decode_little/1]).
+	 decode_little/1,
+	 encode_big/1,
+	 decode_big/1]).
 
 -spec encode_little(list()) -> binary().
 encode_little(List) ->
@@ -15,6 +17,14 @@ encode_little(List) ->
 -spec decode_little(binary()) -> list().
 decode_little(Data) ->
     decode_little(Data, []).
+
+-spec encode_big(list()) -> binary().
+encode_big(List) ->
+    encode_big(List, <<>>).
+
+-spec decode_big(binary()) -> list().
+decode_big(Data) ->
+    decode_big(Data, []).
 
 %%==============================================================================
 %% Private
@@ -31,12 +41,27 @@ decode_little(<<>>, Acc) ->
 decode_little(<<Val:32/little, Rest/bitstring>>, Acc) ->
     decode_little(Rest, [Val|Acc]).
 
+-spec encode_big(list(), binary()) -> binary().
+encode_big([], Acc) ->
+    Acc;
+encode_big([H|T], Acc) ->
+    encode_big(T, <<Acc/binary, H:32/big>>).
+
+decode_big(<<>>, Acc) ->
+    lists:reverse(Acc);
+decode_big(<<Val:32/big, Rest/bitstring>>, Acc) ->
+    decode_big(Rest, [Val|Acc]).
+
 %%==============================================================================
 %% Test
 %%==============================================================================
 
 -include_lib("eunit/include/eunit.hrl").
 
-encode_decode_test_() ->
+encode_decode_little_test_() ->
     Data = [1, 2, 3, 4, 5],
     [ ?_assertEqual(decode_little(encode_little(Data)), Data) ].
+
+encode_decode_big_test_() ->
+    Data = [1, 2, 3, 4, 5],
+    [ ?_assertEqual(decode_big(encode_big(Data)), Data) ].
