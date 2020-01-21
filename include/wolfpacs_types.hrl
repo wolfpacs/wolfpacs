42d93218aa461eb10d95967db25d95c8089b5581
Niklas Johansson <raphexion@gmail.com>
2020-01-21 20:21:50 +0100

    types: Add types to pdv record

--- include/wolfpacs_types.hrl
+++ include/wolfpacs_types.hrl
@@ -1,4 +1,4 @@
 -type decoded() :: {ok, binary(), binary()} | {error, binary()}.
 -type decoded_list() :: {ok, list(binary()), binary()} | {error, binary()}.
 
--record(pdv_item, {pr_cid, is_last, is_command, pdv_data}).
+-record(pdv_item, {pr_cid :: integer(), is_last :: boolean(), is_command :: boolean(), pdv_data :: binary()}).
