1ef882a7561c8ebb2bc4d5faa27e6026221a3aac
Niklas Johansson <raphexion@gmail.com>
2020-01-25 19:29:06 +0100

    Add abstract_syntax include file

---
+++ include/abstract_syntax.hrl
@@ -0,0 +1,4 @@
+-define(VERIFICATION, <<"1.2.840.10008.1.1">>).
+-define(SECONDARY_CAPTURE, <<"1.2.840.10008.5.1.4.1.1.7">>).
+-define(CT_IMAGE_STORAGE, <<"1.2.840.10008.5.1.4.1.1.2">>).
+-define(PRINT_JOB, <<"1.2.840.10008.5.1.1.14">>).
