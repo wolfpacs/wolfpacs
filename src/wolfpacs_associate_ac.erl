72fa47c20abdcadfabaf7d0d81013f52f53d3a0a
Niklas Johansson <raphexion@gmail.com>
2020-01-29 19:22:30 +0100

    associate: Add more unit tests

--- src/wolfpacs_associate_ac.erl
+++ src/wolfpacs_associate_ac.erl
@@ -128,6 +128,7 @@ encode_decode_test_() ->
     Incorrect0 = wolfpacs_utils:drop_last_byte(Encoded0),
     Incorrect1 = wolfpacs_utils:drop_first_byte(Encoded0),
     Incorrect2 = <<1, 2, 3, 4, 5>>,
+    Incorrect3 = binary:replace(Encoded0, <<"1.2.840">>, <<>>),
 
     Correct0 = {ok, CalledAE, CallingAE, R, [{PrCID, TransferSyntax}], MaxPDUSize, Class, VersionName, <<>>},
     Correct1 = {ok, CalledAE, CallingAE, R, [{PrCID, TransferSyntax}], MaxPDUSize, Class, VersionName, <<42>>},
@@ -137,4 +138,5 @@ encode_decode_test_() ->
     , ?_assertEqual(decode(Incorrect0), {error, Incorrect0})
     , ?_assertEqual(decode(Incorrect1), {error, Incorrect1})
     , ?_assertEqual(decode(Incorrect2), {error, Incorrect2})
+    , ?_assertEqual(decode(Incorrect3), {error, Incorrect3})
     ].
--- src/wolfpacs_associate_rq.erl
+++ src/wolfpacs_associate_rq.erl
@@ -131,7 +131,9 @@ encode_decode_test_() ->
     Encoded0 = encode(CalledAE, CallingAE, PrCID, AbstractSyntax, TransferSyntax, MaxPDUSize, Class, VersionName),
     Encoded1 = <<Encoded0/binary, 42>>,
     Incorrect0 = wolfpacs_utils:drop_last_byte(Encoded0),
-    Incorrect1 = <<1, 2, 3, 4>>,
+    Incorrect1 = wolfpacs_utils:drop_first_byte(Encoded0),
+    Incorrect2 = <<1, 2, 3, 4>>,
+    Incorrect3 = binary:replace(Encoded0, <<"1.2.840">>, <<>>),
 
     [?_assertEqual(decode(Encoded0), {ok, CalledAE, CallingAE, R,
 				      [{PrCID, AbstractSyntax, TransferSyntax}],
@@ -142,5 +144,7 @@ encode_decode_test_() ->
 				      MaxPDUSize, Class, VersionName,
 				      <<42>>}),
      ?_assertEqual(decode(Incorrect0), {error,  Incorrect0}),
-     ?_assertEqual(decode(Incorrect1), {error,  Incorrect1})
+     ?_assertEqual(decode(Incorrect1), {error,  Incorrect1}),
+     ?_assertEqual(decode(Incorrect2), {error,  Incorrect2}),
+     ?_assertEqual(decode(Incorrect3), {error,  Incorrect3})
     ].
