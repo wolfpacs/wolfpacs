b87119d8ef361d66a65537222c4541bc09ab47d9
Niklas Johansson <raphexion@gmail.com>
2020-02-29 08:53:36 +0100

    data_elements: Add more proper tests

--- test/wolfpacs_test_generators.erl
+++ test/wolfpacs_test_generators.erl
@@ -1,13 +1,22 @@
 -module(wolfpacs_test_generators).
 -include_lib("proper/include/proper.hrl").
--export([strategy/0]).
+-export([strategy/0,
+	 valid_strategy/0]).
 
 strategy() ->
     Available = [ {explicit, little}
 		, {explicit, big}
- 		, {implicit, little}
+		, {implicit, little}
 		, {implicit, big}
 		, badStrategy
 		, {badType, badEndian}
+		],
+    oneof(Available).
+
+valid_strategy() ->
+    Available = [ {explicit, little}
+		, {explicit, big}
+		, {implicit, little}
+		, {implicit, big}
 		],
     oneof(Available).
