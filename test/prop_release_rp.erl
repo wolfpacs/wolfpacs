545d8fc758964efff55c63c020e8d18a640d6f26
Niklas Johansson <raphexion@gmail.com>
2020-03-11 20:06:21 +0100

    release_rq: Add fuzzing test

---
+++ test/prop_release_rp.erl
@@ -0,0 +1,41 @@
+-module(prop_release_rp).
+-include_lib("proper/include/proper.hrl").
+
+%%%%%%%%%%%%%%%%%%
+%%% Properties %%%
+%%%%%%%%%%%%%%%%%%
+prop_random_clear_test() ->
+    ?FORALL(_, term(),
+	    begin
+		Encoded = wolfpacs_release_rp:encode(),
+		Corrupt = wolfpacs_utils:random_clear(Encoded, 0.2),
+		case wolfpacs_release_rp:decode(Corrupt) of
+		    {ok, _, _} ->
+			true;
+		    {error, Corrupt, _} ->
+			true;
+		    _ ->
+			false
+		end
+	    end).
+
+prop_decode_test() ->
+    ?FORALL(Data, binary(),
+	    begin
+		case wolfpacs_release_rp:decode(Data) of
+		    {ok, _, _} ->
+			true;
+		    {error, Data, _} ->
+			true;
+		    _ ->
+			false
+		end
+	    end).
+
+%%%%%%%%%%%%%%%
+%%% Helpers %%%
+%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%
+%%% Generators %%%
+%%%%%%%%%%%%%%%%%%
