bdffbdcab41a1dc6566d1f93c393500a0899387e
Niklas Johansson <raphexion@gmail.com>
2020-06-01 20:42:50 +0200

    Refactor prop test vr_da to use flow

--- test/prop_vr_da.erl
+++ test/prop_vr_da.erl
@@ -9,12 +9,12 @@ prop_random_clear_test() ->
 	    begin
  		Strategy = {explicit, little},
 		Info = <<1, 2, 3, 4>>,
-		Encoded = wolfpacs_vr_da:encode(Strategy, Info),
+		Encoded = wolfpacs_vr_da:encode(no_flow, Strategy, Info),
 		Corrupt = wolfpacs_utils:random_clear(Encoded, 0.2),
-		case wolfpacs_vr_da:decode(Strategy, Corrupt) of
+		case wolfpacs_vr_da:decode(no_flow, Strategy, Corrupt) of
 		    {ok, _, _} ->
 			true;
-		    {error, Corrupt, _} ->
+		    error ->
 			true;
 		    _ ->
 			false
@@ -25,10 +25,10 @@ prop_decode_test() ->
     ?FORALL(Data, binary(),
 	    begin
  		Strategy = {explicit, little},
-		case wolfpacs_vr_da:decode(Strategy, Data) of
+		case wolfpacs_vr_da:decode(no_flow, Strategy, Data) of
 		    {ok, _, _} ->
 			true;
-		    {error, Data, _} ->
+		    error ->
 			true;
 		    _ ->
 			false
