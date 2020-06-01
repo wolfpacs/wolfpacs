0d8b61b65dd1e458d70009bfa1e49b83df63661d
Niklas Johansson <raphexion@gmail.com>
2020-06-01 20:37:47 +0200

    Refactor vr ae to use flow

--- src/wolfpacs_vr_ae.erl
+++ src/wolfpacs_vr_ae.erl
@@ -9,54 +9,10 @@
 %%%-------------------------------------------------------------------
 
 -module(wolfpacs_vr_ae).
--export([encode/2, decode/2]).
--include("wolfpacs_types.hrl").
--import(wolfpacs_vr_utils, [pad_binary/1,
-			    limit_binary/2,
-			    trim_binary/1]).
+-export([encode/3, decode/3]).
 
--spec encode(strategy(), binary()) -> binary().
-encode(_Strategy, AE) ->
-    encode(AE).
+encode(Flow, _Strategy, AE) ->
+    wolfpacs_vr_common:encode_with_limit(Flow, ?MODULE, 16, AE).
 
--spec decode(strategy(), binary()) -> {ok, binary(), binary()} | {error, binary(), list(string())}.
-decode(_Strategy, Data) ->
-    decode(Data).
-
-%%==============================================================================
-%% Private
-%%==============================================================================
-
--spec encode(list() | binary()) -> binary().
-encode(UI) when is_list(UI) ->
-    encode(list_to_binary(UI));
-encode(UI) ->
-    limit_binary(pad_binary(UI), 16).
-
--spec decode(binary()) -> binary().
-decode(<<>>) ->
-    {error, <<>>, ["empty AE"]};
-decode(Data) ->
-    {ok, trim_binary(Data), <<>>}.
-
-%%==============================================================================
-%% Test
-%%==============================================================================
-
--include_lib("eunit/include/eunit.hrl").
-
-encode_test_() ->
-    [?_assertEqual(encode(""),     <<"">>),
-     ?_assertEqual(encode("A"),    <<"A", 0>>),
-     ?_assertEqual(encode("AB"),   <<"AB">>),
-     ?_assertEqual(encode("12345678901234567890"),   <<"1234567890123456">>) ].
-
-encode_decode_test_() ->
-    Long = [$A || _ <- lists:seq(1, 32)],
-    Trimmed = list_to_binary([$A || _ <- lists:seq(1, 16)]),
-    [?_assertEqual(decode(encode("")), {error, <<>>, ["empty AE"]}),
-     ?_assertEqual(decode(encode("A")), {ok, <<"A">>, <<>>}),
-     ?_assertEqual(decode(encode("AB")), {ok, <<"AB">>, <<>>}),
-     ?_assertEqual(decode(encode("ABC")), {ok, <<"ABC">>, <<>>}),
-     ?_assertEqual(decode(encode("ABCD")), {ok, <<"ABCD">>, <<>>}),
-     ?_assertEqual(decode(encode(Long)), {ok, Trimmed, <<>>}) ].
+decode(Flow, _Strategy, Data) ->
+    wolfpacs_vr_common:decode(Flow, ?MODULE, Data).
