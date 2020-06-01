46fbb24cd025bef598bc84ad3d3b93d6b13d2a0b
Niklas Johansson <raphexion@gmail.com>
2020-06-01 20:37:47 +0200

    Refactor vr un to use flow

--- src/wolfpacs_vr_un.erl
+++ src/wolfpacs_vr_un.erl
@@ -5,53 +5,11 @@
 %%%-------------------------------------------------------------------
 
 -module(wolfpacs_vr_un).
--export([encode/2,
-	 decode/2]).
--import(wolfpacs_vr_utils, [pad_binary/1,
-			    trim_binary/1]).
+-export([encode/3,
+	 decode/3]).
 
--type un() :: list() | binary().
+encode(Flow, _Strategy, UN) ->
+     wolfpacs_vr_common:encode(Flow, ?MODULE, UN).
 
-encode(_Strategy, UN) ->
-    encode(UN).
-
-decode(_Strategy, UN) ->
-    decode(UN).
-
-%%==============================================================================
-%% Private
-%%==============================================================================
-
--spec encode(un()) -> binary().
-encode(UN) when is_list(UN) ->
-    encode(list_to_binary(UN));
-encode(UN) ->
-    pad_binary(UN).
-
--spec decode(binary()) -> binary().
-decode(<<>>) ->
-    {error, <<>>, ["empty UN"]};
-decode(Data) ->
-    {ok, trim_binary(Data), <<>>}.
-
-%%==============================================================================
-%% Test
-%%==============================================================================
-
--include_lib("eunit/include/eunit.hrl").
-
-encode_test_() ->
-    [?_assertEqual(encode(""), <<"">>),
-     ?_assertEqual(encode("A"), <<"A", 0>>),
-     ?_assertEqual(encode("AB"), <<"AB">>),
-     ?_assertEqual(encode("ABC"), <<"ABC", 0>>),
-     ?_assertEqual(encode("ABCD"), <<"ABCD">>) ].
-
-encode_decode_test_() ->
-    Long = list_to_binary([$A || _ <- lists:seq(1, 1024)]),
-    [?_assertEqual(decode(encode("")), {error, <<>>, ["empty UN"]}),
-     ?_assertEqual(decode(encode("A")), {ok, <<"A">>, <<>>}),
-     ?_assertEqual(decode(encode("AB")), {ok, <<"AB">>, <<>>}),
-     ?_assertEqual(decode(encode("ABC")), {ok, <<"ABC">>, <<>>}),
-     ?_assertEqual(decode(encode("ABCD")), {ok, <<"ABCD">>, <<>>}),
-     ?_assertEqual(decode(encode(Long)), {ok, Long, <<>>}) ].
+decode(Flow, _Strategy, UN) ->
+    wolfpacs_vr_common:decode(Flow, ?MODULE, UN).
