9bb6c207b3bb129d5b3ebdcdc67ede764cac6edb
Niklas Johansson <raphexion@gmail.com>
2020-06-03 18:35:18 +0200

    vr_as: Improve test coverage

--- src/wolfpacs_vr_as.erl
+++ src/wolfpacs_vr_as.erl
@@ -16,9 +16,65 @@
 
 -module(wolfpacs_vr_as).
 -export([encode/3, decode/3]).
+-include("wolfpacs_types.hrl").
+-import(wolfpacs_vr_utils, [limit_binary/2]).
 
-encode(Flow, _Strategy, AS) ->
-    wolfpacs_vr_common:encode_with_limit(Flow, ?MODULE, 4, AS).
+-spec encode(flow(), strategy(), binary()) -> binary().
+encode(_Flow, _Strategy, AE) ->
+    encode(AE).
 
-decode(Flow, _Strategy, Data) ->
-    wolfpacs_vr_common:decode(Flow, ?MODULE, Data).
+-spec decode(flow(), strategy(), binary()) -> {ok, binary(), binary()} | {error, binary(), list(string())}.
+decode(_Flow, _Strategy, Data) ->
+    decode(Data).
+
+%%==============================================================================
+%% Private
+%%==============================================================================
+
+-spec encode(list() | binary()) -> binary().
+encode(AS) when is_list(AS) ->
+    encode(list_to_binary(AS));
+encode(AS) ->
+    limit_binary(AS, 4).
+
+-spec decode(binary()) -> binary().
+decode(<<>>) ->
+    error;
+decode(<<A, B, C, "D", Rest/binary>>) ->
+    {ok, <<A, B, C, "D">>, Rest};
+decode(<<A, B, C, "W", Rest/binary>>) ->
+    {ok, <<A, B, C, "W">>, Rest};
+decode(<<A, B, C, "M", Rest/binary>>) ->
+    {ok, <<A, B, C, "M">>, Rest};
+decode(<<A, B, C, "Y", Rest/binary>>) ->
+    {ok, <<A, B, C, "Y">>, Rest};
+decode(_Data) ->
+    error.
+
+%%==============================================================================
+%% Test
+%%==============================================================================
+
+-include_lib("eunit/include/eunit.hrl").
+
+encode_test_() ->
+    {ok, Flow} = wolfpacs_flow:start_link(),
+    [ ?_assertEqual(encode("018M"), <<"018M">>)
+    , ?_assertEqual(encode(Flow, {explicit, little}, "018M"), <<"018M">>)
+    ].
+
+decode_test_() ->
+    {ok, Flow} = wolfpacs_flow:start_link(),
+    [ ?_assertEqual(decode(<<>>), error)
+    , ?_assertEqual(decode(<<"ABCQ">>), error)
+    , ?_assertEqual(decode(<<"123WABC">>), {ok, <<"123W">>, <<"ABC">>})
+    , ?_assertEqual(decode(Flow, {explicit, little}, <<"123WABC">>), {ok, <<"123W">>, <<"ABC">>})
+    ].
+
+encode_decode_test_() ->
+    [ ?_assertEqual(decode(encode("123D")), {ok, <<"123D">>, <<>>})
+    , ?_assertEqual(decode(encode("123W")), {ok, <<"123W">>, <<>>})
+    , ?_assertEqual(decode(encode("123M")), {ok, <<"123M">>, <<>>})
+    , ?_assertEqual(decode(encode("123Y")), {ok, <<"123Y">>, <<>>})
+    , ?_assertEqual(decode(encode("123Y")), {ok, <<"123Y">>, <<>>})
+    ].
