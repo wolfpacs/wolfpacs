cc8b637c325a541c75796cf75dd598c34481dec2
Niklas Johansson <raphexion@gmail.com>
2020-07-06 17:40:31 +0200

    associate_rq: Add table in doc header

--- src/wolfpacs_associate_rq.erl
+++ src/wolfpacs_associate_rq.erl
@@ -1,6 +1,63 @@
 %%%-------------------------------------------------------------------
 %% @doc Associate Request (RQ)
 %%
+%% <table>
+%%   <thead>
+%%     <tr>
+%%       <th>PDU bytes</th>
+%%       <th>Field name</th>
+%%       <th>Description of field</th>
+%%     </tr>
+%%   </thead>
+%%   <tbody>
+%%     <tr>
+%%       <td>1</td>
+%%       <td>PDU-type</td>
+%%       <td>01H</td>
+%%     </tr>
+%%     <tr>
+%%       <td>2</td>
+%%       <td>Reserved</td>
+%%       <td>This reserved field shall be sent with a value 00H but not tested to this value when received.</td>
+%%     </tr>
+%%     <tr>
+%%       <td>3-6</td>
+%%       <td>PDU-length</td>
+%%       <td>This PDU-length shall be the number of bytes from the first byte of the following field to the last byte of the variable field. It shall be encoded as an unsigned binary number</td>
+%%     </tr>
+%%     <tr>
+%%       <td>7-8</td>
+%%       <td>Protocol-version</td>
+%%       <td>This two byte field shall use one bit to identify each version of the DICOM UL protocol supported by the calling end-system. This is Version 1 and shall be identified with bit 0 set. A receiver of this PDU implementing only this version of the DICOM UL protocol shall only test that bit 0 is set.</td>
+%%     </tr>
+%%     <tr>
+%%       <td>9-10</td>
+%%       <td>Reserved</td>
+%%       <td>This reserved field shall be sent with a value 0000H but not tested to this value when received.</td>
+%%     </tr>
+%%     <tr>
+%%       <td>11-26</td>
+%%       <td>Called-AE-title</td>
+%%       <td>Destination DICOM Application Name. It shall be encoded as 16 characters as defined by the ISO 646:1990-Basic G0 Set with leading and trailing spaces (20H) being non-significant. The value made of 16 spaces (20H) meaning "no Application Name specified" shall not be used. For a complete description of the use of this field, see Section 7.1.1.4.</td>
+%%     </tr>
+%%     <tr>
+%%       <td>27-42</td>
+%%       <td>Calling-AE-title</td>
+%%       <td>Source DICOM Application Name. It shall be encoded as 16 characters as defined by the ISO 646:1990-Basic G0 Set with leading and trailing spaces (20H) being non-significant. The value made of 16 spaces (20H) meaning "no Application Name specified" shall not be used. For a complete description of the use of this field, see Section 7.1.1.3.</td>
+%%     </tr>
+%%     <tr>
+%%       <td>43-74</td>
+%%       <td>Reserved</td>
+%%       <td>This reserved field shall be sent with a value 00H for all bytes but not tested to this value when received</td>
+%%     </tr>
+%%     <tr>
+%%       <td>75-xxx</td>
+%%       <td>Variable items</td>
+%%       <td>This variable field shall contain the following items: one Application Context Item, one or more Presentation Context Items and one User Information Item. For a complete description of the use of these items see Section 7.1.1.2, Section 7.1.1.13, and Section 7.1.1.6.</td>
+%%     </tr>
+%%   </tbody>
+%% </table>
+%%
 %% @end
 %%%-------------------------------------------------------------------
 
