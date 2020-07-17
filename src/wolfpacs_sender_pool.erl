1c2306e78eb312effd22b3b919d0991029685dba
Niklas Johansson <raphexion@gmail.com>
2020-07-17 14:12:31 +0200

    Add sender pool

---
+++ src/wolfpacs_sender_pool.erl
@@ -0,0 +1,123 @@
+-module(wolfpacs_sender_pool).
+-behaviour(gen_server).
+-define(SERVER, ?MODULE).
+-define(PARALLEL_SENDS, 25).
+
+-export([start_link/0,
+	 stop/0,
+	 send/5,
+	 done/0,
+	 size/0]).
+
+-export([init/1,
+	 handle_call/3,
+	 handle_cast/2,
+	 handle_info/2,
+	 terminate/2,
+	 code_change/3]).
+
+%%-----------------------------------------------------------------------------
+%% API
+%%------------------------------------------------------------------------------
+
+-record(sender_info, {host :: string(),
+		      port :: integer(),
+		      called :: binary(),
+		      calling :: binary(),
+		      dataset :: map()
+		     }).
+
+start_link() ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+stop() ->
+    gen_server:stop(?MODULE).
+
+send(Host, Port, CalledAE, CallingAE, DataSet) ->
+    SenderInfo = #sender_info{host = s(Host),
+			      port = i(Port),
+			      called = b(CalledAE),
+			      calling = b(CallingAE),
+			      dataset = DataSet},
+    gen_server:cast(?MODULE, {send, SenderInfo}).
+
+done() ->
+    gen_server:cast(?MODULE, done).
+
+size() ->
+    gen_server:call(?MODULE, size).
+
+%%-----------------------------------------------------------------------------
+%% Behaviour callbacks
+%%------------------------------------------------------------------------------
+
+init(_) ->
+    Queue = queue:new(),
+    {ok, Queue}.
+
+handle_call(size, _From, Queue) ->
+    {reply, {ok, queue:len(Queue)}, Queue};
+
+handle_call(What, _From, Queue) ->
+    {reply, {error, What}, Queue}.
+
+handle_cast({send, SenderInfo}, Queue) ->
+    {noreply, queue:in(SenderInfo, Queue), 0};
+
+handle_cast(done, Queue) ->
+    Len = queue:len(Queue),
+    handle_timeout(Queue, Len == 0, Len < ?PARALLEL_SENDS);
+
+handle_cast(_What, Queue) ->
+    {noreply, Queue}.
+
+handle_info(timeout, Queue) ->
+    Len = queue:len(Queue),
+    handle_timeout(Queue, Len == 0, Len < ?PARALLEL_SENDS);
+
+handle_info(_What, Queue) ->
+    {noreply, Queue}.
+
+terminate(_Reason, _Queue) ->
+    ok.
+
+code_change(_Vsn, Queue, _Extra) ->
+    {ok, Queue}.
+
+%%==============================================================================
+%% Private
+%%==============================================================================
+
+handle_timeout(Queue, true, _) ->
+    {noreply, Queue, hibernate};
+handle_timeout(QueueIn, false, true) ->
+    {{value, SenderInfo}, QueueOut} = queue:out(QueueIn),
+    spawn(fun() ->
+		  #sender_info{host = Host,
+			       port = Port,
+			       called = CalledAE,
+			       calling = CallingAE,
+			       dataset = DataSet} = SenderInfo,
+		  {ok, Sender} = wolfpacs_sender:start_link(Host, Port, CalledAE, CallingAE),
+		  wolfpacs_sender:send(Sender, DataSet),
+		  wolfpacs_sender:stop(Sender),
+		  done()
+	  end),
+    {noreply, QueueOut, 0};
+handle_timeout(Queue, _, _) ->
+    {noreply, Queue, 100}.
+
+i(Value) when is_list(Value) ->
+    list_to_integer(Value);
+i(Value) when is_integer(Value) ->
+    Value.
+
+b(Value) when is_list(Value) ->
+    list_to_binary(Value);
+b(Value) when is_binary(Value) ->
+    Value.
+
+s(Value) when is_binary(Value) ->
+    binary_to_list(Value);
+s(Value) when is_list(Value) ->
+    Value.
