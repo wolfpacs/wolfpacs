242a8e87d8f5529df6ca34c9892069eefe5ffd4d
Niklas Johansson <raphexion@gmail.com>
2020-07-17 08:35:57 +0200

    test/testutils: Add read_dataset with flow

--- test/testutils.erl
+++ test/testutils.erl
@@ -1,8 +1,12 @@
 -module(testutils).
--export([read_dataset/1]).
+-export([read_dataset/1,
+	 read_dataset/2]).
 
 read_dataset(Filename) ->
     {ok, Flow} = wolfpacs_flow:start_link(),
+    read_dataset(Flow, Filename).
+
+read_dataset(Flow, Filename) ->
     Strategy = {explicit, little},
     {ok, Content} = file:read_file(Filename),
     {ok, {_Meta, Info}, <<>>} = wolfpacs_file_format:decode(Flow, Strategy, Content),
