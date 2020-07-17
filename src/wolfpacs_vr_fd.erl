d38860b2a9585d896047b85ce2bf68c5feda1e23
Niklas Johansson <raphexion@gmail.com>
2020-07-17 10:40:48 +0200

    Add Double Float (VR)

---
+++ src/wolfpacs_vr_fd.erl
@@ -0,0 +1,82 @@
+
+%%%-------------------------------------------------------------------
+%% @doc Value Representation Float Double (FD).
+%%
+%% Double precision binary floating point number represented in
+%% IEEE 754:1985 64-bit Floating Point Number Format.
+%%
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(wolfpacs_vr_fd).
+-export([encode/3, decode/3]).
+
+-include("wolfpacs_types.hrl").
+
+-define(BYTE_SIZE, 8).
+-define(BIT_SIZE, (?BYTE_SIZE * 8)).
+
+encode(Flow, Strategy, Values) when is_list(Values) ->
+    Parts = [ encode(Flow, Strategy, Value) || Value <- Values ],
+    F = fun(Part, Acc) -> <<Acc/binary, Part/binary>> end,
+    lists:foldl(F, <<>>, Parts);
+
+encode(Flow, {_, little}, X) ->
+    wolfpacs_flow:generated(Flow, ?MODULE, ?BYTE_SIZE),
+    <<X:?BIT_SIZE/float-little>>;
+
+encode(Flow, {_, big}, X) ->
+    wolfpacs_flow:generated(Flow, ?MODULE, ?BYTE_SIZE),
+    <<X:?BIT_SIZE/float-big>>.
+
+decode(Flow, Strategy, Data) when byte_size(Data) > ?BYTE_SIZE ->
+    Parts = wolfpacs_utils:chunk(Data, ?BYTE_SIZE),
+    F = fun(Part) -> decode(Flow, Strategy, Part) end,
+    wolfpacs_utils:flatten_decoded(lists:map(F, Parts));
+
+decode(Flow, {_, little}, <<X:?BIT_SIZE/float-little>>) ->
+    wolfpacs_flow:consumed(Flow, ?MODULE, ?BYTE_SIZE),
+    {ok, X, <<>>};
+
+decode(Flow, {_, big}, <<X:?BIT_SIZE/float-big>>) ->
+    wolfpacs_flow:consumed(Flow, ?MODULE, ?BYTE_SIZE),
+    {ok, X, <<>>};
+
+decode(Flow, _, _) ->
+    wolfpacs_flow:failed(Flow, ?MODULE, "unable to decode"),
+    error.
+
+%%==============================================================================
+%% Test
+%%==============================================================================
+
+-include_lib("eunit/include/eunit.hrl").
+
+encode_decode(Flow, Strategy, Value) ->
+    Encoded = encode(Flow, Strategy, Value),
+    Decoded = decode(Flow, Strategy, Encoded),
+    ?_assertEqual(Decoded, {ok, Value, <<>>}).
+
+encode_decode_all_strategies(Flow, Value) ->
+    [ encode_decode(Flow, {explicit, little}, Value)
+    , encode_decode(Flow, {explicit, big}, Value)
+    , encode_decode(Flow, {implicit, little}, Value)
+    , encode_decode(Flow, {implicit, big}, Value)
+    ].
+
+encode_decode_test_() ->
+    {ok, Flow} = wolfpacs_flow:start_link(),
+    lists:flatten(
+      [ encode_decode_all_strategies(Flow, 1.0)
+      , encode_decode_all_strategies(Flow, -1.0)
+      , encode_decode_all_strategies(Flow, 12.12)
+      ]
+     ).
+
+encode_decode_vm_test_() ->
+    S = {explicit, little},
+    Values = [1.0, -1.0, 1024.0],
+    Encoded = encode(no_flow, S, Values),
+    Result = decode(no_flow, S, Encoded),
+    [ ?_assertEqual(Result, {ok, Values, <<>>})
+    ].
---
+++ test/prop_vr_fd.erl
@@ -0,0 +1,43 @@
+-module(prop_vr_fd).
+-include_lib("proper/include/proper.hrl").
+
+%%%%%%%%%%%%%%%%%%
+%%% Properties %%%
+%%%%%%%%%%%%%%%%%%
+prop_random_clear_test() ->
+    ?FORALL(Info, float(),
+	    begin
+		Strategy = {explicit, little},
+		Encoded = wolfpacs_vr_fd:encode(no_flow, Strategy, Info),
+		Corrupt = wolfpacs_utils:random_clear(Encoded, 0.2),
+		case wolfpacs_vr_fd:decode(no_flow, Strategy, Corrupt) of
+		    {ok, _, _} ->
+			true;
+		    error ->
+			true;
+		    _ ->
+			false
+		end
+	    end).
+
+prop_decode_test() ->
+    ?FORALL(Data, float(),
+	    begin
+		Strategy = wolfpacs_test_generators:valid_strategy(),
+		case wolfpacs_vr_fd:decode(no_flow, Strategy, Data) of
+		    {ok, _, _} ->
+			true;
+		    error ->
+			true;
+		    _ ->
+			false
+		end
+	    end).
+
+%%%%%%%%%%%%%%%
+%%% Helpers %%%
+%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%
+%%% Generators %%%
+%%%%%%%%%%%%%%%%%%
