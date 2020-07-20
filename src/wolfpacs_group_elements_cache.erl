dcb06a1040e7ec7aacb920cbbc435ecb2f0aa1f9
Niklas Johansson <raphexion@gmail.com>
2020-07-20 06:33:11 +0200

    Add group element cache

---
+++ src/wolfpacs_group_elements_cache.erl
@@ -0,0 +1,97 @@
+-module(wolfpacs_group_elements_cache).
+-behaviour(gen_server).
+-define(SERVER, ?MODULE).
+
+-export([start_link/0,
+	 stop/0,
+	 add/3,
+	 get/2,
+	 size/0,
+	 purge/0]).
+
+-export([init/1,
+	 handle_call/3,
+	 handle_cast/2,
+	 handle_info/2,
+	 terminate/2,
+	 code_change/3]).
+
+%%-----------------------------------------------------------------------------
+%% API
+%%------------------------------------------------------------------------------
+
+start_link() ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+stop() ->
+    gen_server:stop(?MODULE).
+
+add(Group, Element, VR) ->
+    gen_server:cast(?MODULE, {add, {Group, Element}, VR}).
+
+get(Group, Element) ->
+    try gen_server:call(?MODULE, {get, {Group, Element}}) of
+	Res -> Res
+    catch
+	_:_ ->
+	    "UN"
+    end.
+
+size() ->
+    gen_server:call(?MODULE, size).
+
+purge() ->
+    gen_server:cast(?MODULE, purge).
+
+%%-----------------------------------------------------------------------------
+%% Behaviour callbacks
+%%------------------------------------------------------------------------------
+
+init(_) ->
+    {ok, #{}}.
+
+handle_call({get, Key}, _From, Cache) ->
+    Value = maps:get(Key, Cache, "UN"),
+    {reply, {ok, Value}, Cache};
+handle_call(size, _From, Cache) ->
+    {reply, {ok, maps:size(Cache)}, Cache};
+
+handle_call(What, _From, State) ->
+    {reply, {error, What}, State}.
+
+handle_cast({add, Key, Value}, Cache) ->
+    {noreply, Cache#{Key => Value}};
+
+handle_cast(purge, _Cache) ->
+    {noreply, #{}};
+
+handle_cast(_What, State) ->
+    {noreply, State}.
+
+handle_info(_What, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_Vsn, State, _Extra) ->
+    {ok, State}.
+
+%%==============================================================================
+%% Test
+%%==============================================================================
+
+-include_lib("eunit/include/eunit.hrl").
+
+minimal_test_() ->
+    _ = start_link(),
+    [ ?_assertEqual(size(), {ok, 0})
+    , ?_assertEqual(add(1, 2, "AE"), ok)
+    , ?_assertEqual(size(), {ok, 1})
+    , ?_assertEqual(add(1, 2, "AE"), ok)
+    , ?_assertEqual(size(), {ok, 1})
+    , ?_assertEqual(add(3, 4, "AS"), ok)
+    , ?_assertEqual(size(), {ok, 2})
+    , ?_assertEqual(purge(), ok)
+    , ?_assertEqual(size(), {ok, 0})
+    ].
