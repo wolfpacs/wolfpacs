71175b4c2d27dc95cf11565f89e8dd11711da2f8
Niklas Johansson <raphexion@gmail.com>
2020-07-22 12:05:19 +0200

    SQ Context Request

--- test/prop_presentation_context_request.erl
+++ test/prop_presentation_context_request.erl
@@ -14,12 +14,12 @@ prop_test() ->
 		TransferSyntax = [?IMPLICIT_LITTLE_ENDIAN,
 				  ?EXPLICIT_LITTLE_ENDIAN,
 				  ?EXPLICIT_BIG_ENDIAN],
-		Encoded = wolfpacs_presentation_context_request:encode(PrCID, AbstractSyntax, TransferSyntax),
+		Encoded = wolfpacs_presentation_context_request:encode(no_flow, PrCID, AbstractSyntax, TransferSyntax),
 		Corrupt = wolfpacs_utils:random_clear(Encoded, 0.20),
-		case wolfpacs_presentation_context_request:decode(Corrupt) of
+		case wolfpacs_presentation_context_request:decode(no_flow, Corrupt) of
 		    {ok, _, _, _, _} ->
 			true;
-		    {error, Corrupt, _Error} ->
+		    error ->
 			true;
 		    What  ->
 			lager:warning("~p", [What]),
