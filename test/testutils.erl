b09b99039d16960002d6955f61727387d3641f5c
Niklas Johansson <raphexion@gmail.com>
2020-07-26 13:09:03 +0200

    testutils: Add read_testfile helper

--- test/testutils.erl
+++ test/testutils.erl
@@ -1,6 +1,12 @@
 -module(testutils).
--export([read_dataset/1,
+-export([read_testfile/2,
+	 read_dataset/1,
 	 read_dataset/2]).
+
+-include_lib("common_test/include/ct.hrl").
+
+read_testfile(Config, Filename) ->
+    filename:join([?config(data_dir, Config), Filename]).
 
 read_dataset(Filename) ->
     {ok, Flow} = wolfpacs_flow:start_link(),
