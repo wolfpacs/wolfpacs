c7dddede7adc29423ba6d3b9ae0c52779afb7dd1
Niklas Johansson <raphexion@gmail.com>
2020-07-27 22:36:33 +0200

    test: Add propertiy test for vr od

---
+++ test/prop_vr_od.erl
@@ -0,0 +1,43 @@
+-module(prop_vr_od).
+-include_lib("proper/include/proper.hrl").
+
+%%%%%%%%%%%%%%%%%%
+%%% Properties %%%
+%%%%%%%%%%%%%%%%%%
+prop_random_clear_test() ->
+    ?FORALL(Info, list(float()),
+	    begin
+		Strategy = {explicit, little},
+		Encoded = wolfpacs_vr_of:encode(no_flow, Strategy, Info),
+		Corrupt = wolfpacs_utils:random_clear(Encoded, 0.2),
+		case wolfpacs_vr_of:decode(no_flow, Strategy, Corrupt) of
+		    {ok, _, _} ->
+			true;
+		    error ->
+			true;
+		    _ ->
+			false
+		end
+	    end).
+
+prop_decode_test() ->
+    ?FORALL(Data, binary(),
+	    begin
+		Strategy = wolfpacs_test_generators:valid_strategy(),
+		case wolfpacs_vr_of:decode(no_flow, Strategy, Data) of
+		    {ok, _, _} ->
+			true;
+		    error ->
+			true;
+		    _ ->
+			false
+		end
+	    end).
+
+%%%%%%%%%%%%%%%
+%%% Helpers %%%
+%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%
+%%% Generators %%%
+%%%%%%%%%%%%%%%%%%
