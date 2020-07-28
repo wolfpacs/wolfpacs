0c73ba22efdf3ec20fc805042ba533bffaf4df73
Niklas Johansson <raphexion@gmail.com>
2020-07-28 19:11:48 +0200

    authentication: Add more unit tests

--- src/wolfpacs_authentication.erl
+++ src/wolfpacs_authentication.erl
@@ -186,3 +186,17 @@ minimal_called_test_() ->
     , ?_assertEqual(authenticate("C", "D"), {ok, false})
     , ?_assertEqual(stop(), ok)
     ].
+
+cast_test() ->
+    start_link(),
+    gen_server:cast(?MODULE, this_should_not_crash),
+    ?assertEqual(stop(), ok).
+
+info_test() ->
+    start_link(),
+    ?MODULE ! this_should_not_crash,
+    ?assertEqual(stop(), ok).
+
+code_change_test() ->
+    start_link(),
+    ?assertEqual(code_change(1, state, extra), {ok, state}).
