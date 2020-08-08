d38342931f9963d44a81b29bc38f0af559998b7e
Niklas Johansson <raphexion@gmail.com>
2020-08-08 12:50:47 +0200

    vr_un: Guard against incorrect data (list vs binary)

--- src/wolfpacs_vr_un.erl
+++ src/wolfpacs_vr_un.erl
@@ -10,8 +10,10 @@
 
 -include("wolfpacs_types.hrl").
 
-encode(Flow, _Strategy, UN) ->
-    wolfpacs_vr_common:encode(Flow, ?MODULE, UN, 0).
+encode(Flow, _Strategy, UN) when is_binary(UN) ->
+    wolfpacs_vr_common:encode(Flow, ?MODULE, UN, 0);
+encode(Flow, _Strategy, UN) when is_list(UN) ->
+    wolfpacs_vr_common:encode(Flow, ?MODULE, list_to_binary(UN), 0).
 
 decode(Flow, _Strategy, UN) ->
     wolfpacs_vr_common:decode(Flow, ?MODULE, UN).
